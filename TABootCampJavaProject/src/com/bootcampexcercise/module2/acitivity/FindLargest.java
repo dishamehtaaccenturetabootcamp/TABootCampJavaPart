e53274b4e843c33cab8e24bd0519dcd915584744
dishamehtaaccenturebootcamp <disha.k.mehta@accenture.com>
2022-07-25 17:45:03 +0300

    Create FindLargest.java

---
+++ TABootCampJavaProject/src/com/bootcampexcercise/module2/acitivity/FindLargest.java
@@ -0,0 +1,16 @@
+package com.bootcampexcercise.module2.acitivity;
+
+public class FindLargest {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 88;
+
+        if (x > y) {
+            System.out.println("x is largest");
+        } else {
+            System.out.println("y is largest");
+        }
+
+
+    }
+}
