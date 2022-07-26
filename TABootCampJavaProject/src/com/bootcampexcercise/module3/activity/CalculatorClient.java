9db48fa709666a474bc47ea23eced554dfaae136
dishamehtaaccenturebootcamp <disha.k.mehta@accenture.com>
2022-07-26 21:26:54 +0300

    Update CalculatorClient.java

--- TABootCampJavaProject/src/com/bootcampexcercise/module3/activity/CalculatorClient.java
+++ TABootCampJavaProject/src/com/bootcampexcercise/module3/activity/CalculatorClient.java
@@ -2,47 +2,51 @@ package com.bootcampexcercise.module3.activity;
 
 /**
  * @author
- *
  */
 public class CalculatorClient {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		/*Instantiate Normal Calculator. Note that CALC_TYPE_NORMAL is a public static
-		 *final variable defined in Calculator class. 
-		*/
-		Calculator normalCalc = new Calculator(Calculator.CALC_TYPE_BASIC);
-		
-		System.out.println("*** Calculations will be performed using "+normalCalc.getCalculatorType()+ " calculator ***");
-		
-		// Add 
-		int result = normalCalc.add(4, 5);
-		System.out.println("1) Add result is " + result);
-		
-				
-		// Multiply 
-		int[] mul = {2, 3, 4};
-		//TODO Complete the code: result should be equal to the normalCalc multiply function and int array mul should be multiplied.
-		System.out.println("2) Multiplication result is " + result); 
-		
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		//Division
-		//TODO Write code which will use normalCalc and divide function
-		//TODO Divide numbers should be 10 and 2
-		//TODO Divide result should be printed in similar style as multiply and addition
+        /*Instantiate Normal Calculator. Note that CALC_TYPE_NORMAL is a public static
+         *final variable defined in Calculator class.
+         */
+        Calculator normalCalc = new Calculator(Calculator.CALC_TYPE_BASIC);
+
+        System.out.println("*** Calculations will be performed using " + normalCalc.getCalculatorType() + " calculator ***");
+
+        // Add
+        int result = normalCalc.add(4, 5);
+        System.out.println("1) Add result is " + result);
 
 
+        // Multiply
+        int[] mul = {2, 3, 4};
+        //TODO Complete the code: result should be equal to the normalCalc multiply function and int array mul should be multiplied.
+        // Solution
+        result = normalCalc.multiply(mul);
+        System.out.println("2) Multiplication result is " + result);
 
-		//Subtraction
-		//TODO Write code which will use normalCalc and subtract function
-		//TODO Subtract numbers should be 10 and 2
-		//TODO Subtract result should be printed in similar style as multiply and addition
-	}
+
+        //Division
+        //TODO Write code which will use normalCalc and divide function
+        //TODO Divide numbers should be 10 and 2
+        //TODO Divide result should be printed in similar style as multiply and addition
+        // Solution
+        result = normalCalc.divide(10, 2);
+        System.out.println("3) Divide result is " + result);
 
 
-	
+        //Subtraction
+        //TODO Write code which will use normalCalc and subtract function
+        //TODO Subtract numbers should be 10 and 2
+        //TODO Subtract result should be printed in similar style as multiply and addition
+        // Solution
+        result = normalCalc.subtract(10, 2);
+        System.out.println("4) Subtract result is " + result);
+    }
+
 
 }
